9d9e4e38a218e078b18cffe1833ab5002fc200e9
Zerohertz <ohg3417@gmail.com>
2023-08-28 19:05:18 +0900

    :art: Update: Visualization

--- airflow-continuous-training/main.py
+++ airflow-continuous-training/main.py
@@ -5,12 +5,13 @@ import sys
 import matplotlib.pyplot as plt
 import numpy as np
 import requests
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.preprocessing import LabelEncoder
+from sklearn.tree import DecisionTreeClassifier
 
 WEBHOOK = os.environ.get("WEBHOOK")
 CLASSES = eval(os.environ.get("CLASSES"))
 TIME = os.environ.get("TIME")
+
 
 def main():
     data = sys.argv[1]
@@ -37,11 +38,27 @@ def main():
     n_classes = len(CLASSES)
     colors = plt.cm.jet(np.linspace(0, 1, n_classes))
 
-    contour = ax.contourf(XX, YY, Z.reshape(XX.shape), levels=n_classes-1, colors=colors, alpha=0.5)
-    plt.colorbar(contour)
+    ax.contourf(
+        XX, YY, Z.reshape(XX.shape), levels=n_classes - 1, colors=colors, alpha=0.5
+    )
+    scatter = ax.scatter(
+        X[:, 0], X[:, 1], c=y, marker="o", edgecolors="k", cmap=plt.cm.jet
+    )
 
-    scatter = ax.scatter(X[:, 0], X[:, 1], c=y, marker="o", edgecolors="k", cmap=plt.cm.jet)
-    
+    legend_handles = [
+        plt.Line2D(
+            [0],
+            [0],
+            marker="o",
+            color="w",
+            label=CLASSES[i],
+            markersize=10,
+            markerfacecolor=colors[i],
+        )
+        for i in range(n_classes)
+    ]
+    ax.legend(handles=legend_handles, loc="upper right")
+
     plt.xlim(xlim)
     plt.ylim(ylim)
     plt.title(TIME)
