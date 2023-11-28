585d9378490ab8d1d14601c5faf99578b793019b
Zerohertz <ohg3417@gmail.com>
2023-11-28 11:07:48 +0900

    :sparkles: Feat: Connection with NextCloud

--- airflow-stock/main.py
+++ airflow-stock/main.py
@@ -1,44 +1,74 @@
-import json
 import os
 import time
 
-import requests
+import pandas as pd
+import zerohertzLib as zz
 from prettytable import PrettyTable
 from selenium import webdriver
 
 # DISCORD WEBHOOK
 WEBHOOK = os.environ.get("WEBHOOK")
-# 주식 구매 정보
-STOCK = eval(os.environ.get("STOCK"))
-print(STOCK)
 
 
-def get_price(browser, CODE):
-    browser.get(f"https://finance.naver.com/item/main.naver?code={CODE}")
-    element = browser.find_element(
-        "xpath",
-        "/html/body/div[3]/div[2]/div[2]/div[1]/div[5]/table/tbody/tr[1]/td[1]",
+def target_data(data):
+    return data[~data["Buy"].isna() & data["Sell"].isna()]
+
+
+def get_price(browser, name):
+    browser.get(
+        f"https://stock.mk.co.kr/search/list?search_string={name.replace(' ', '+')}"
     )
-    return int(element.text.replace(",", ""))
+    time.sleep(3)
+    stock_name = browser.find_element(
+        "xpath",
+        "/html/body/div/main/main/section/div[2]/div/div/div[1]/section/div/div/div/table/tbody/tr/td[1]/span",
+    ).text
+    price = int(
+        browser.find_element(
+            "xpath",
+            "/html/body/div/main/main/section/div[2]/div/div/div[1]/section/div/div/div/table/tbody/tr/td[2]/span",
+        ).text.replace(",", "")
+    )
+    return stock_name, price
 
 
-def send_discord_message(webhook_url, content):
-    data = {"content": content}
-    headers = {"Content-Type": "application/json"}
-    response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
-    return response
-
-
-def send_discord_messages(webhook_url, contents):
-    headers = {"Content-Type": "application/json"}
-    for content in contents:
-        data = {"content": content}
-        response = requests.post(webhook_url, data=json.dumps(data), headers=headers)
-        time.sleep(1)
-    return response
+def main(discord, browser, target):
+    discord.message("# :chart_with_upwards_trend: STOCK")
+    table = PrettyTable()
+    table.field_names = [
+        "NAME",
+        "Purchase Price",
+        "Current Price",
+        "Quantity",
+        "Profit and Loss (P&L)",
+    ]
+    TOTAL_BUY = TOTAL_EARN = 0
+    for idx, tar in target.iterrows():
+        stock_name, price = get_price(browser, tar["Name"])
+        if tar["Name"] != stock_name:
+            discord.message(
+                f"Index: {idx}\nstock_name: {stock_name}\ntar['Name']: {tar['Name']}"
+            )
+        total = (price - tar["Buy"]) * tar["Quantity"]
+        table.add_row(
+            [
+                stock_name,
+                f"{tar['Buy']:,.0f}",
+                f"{price:,.0f}",
+                f"{tar['Quantity']:,.0f}",
+                f"{total:,.0f}",
+            ]
+        )
+        TOTAL_BUY += tar["Buy"] * tar["Quantity"]
+        TOTAL_EARN += total
+    table.add_row(["TOTAL", f"{TOTAL_BUY:,.0f}", "-", "-", f"{TOTAL_EARN:,.0f}"])
+    discord.message(str(table), codeblock=True)
 
 
 if __name__ == "__main__":
+    df = pd.read_excel("data/Stock.xlsx")
+    target = target_data(df)
+    Discord = zz.api.Discord(WEBHOOK)
     try:
         options = webdriver.ChromeOptions()
         options.add_argument("--headless")
@@ -49,46 +79,8 @@ if __name__ == "__main__":
             "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
         )
         browser = webdriver.Chrome(options)
-        messages = [f"# :chart_with_upwards_trend: STOCK\n"]
-        table = PrettyTable()
-        table.field_names = [
-            "COMPANY NAME",
-            "Purchase Price",
-            "Current Price",
-            "Quantity",
-            "Profit and Loss (P&L)",
-        ]
-        TOTAL_BUY = 0
-        TOTAL_EARN = 0
-        for COMPANY, CODE, PRICE, QUANTITY in STOCK:
-            if len(str(table)) > 1500:
-                messages.append("```\n" + str(table) + "\n```")
-                table = PrettyTable()
-                table.field_names = [
-                    "COMPANY NAME",
-                    "Purchase Price",
-                    "Current Price",
-                    "Quantity",
-                    "Profit and Loss (P&L)",
-                ]
-            tmp = get_price(browser, CODE)
-            total = (tmp - PRICE) * QUANTITY
-            table.add_row([COMPANY, f"{PRICE:,}", f"{tmp:,}", QUANTITY, f"{total:,}"])
-            TOTAL_BUY += PRICE * QUANTITY
-            TOTAL_EARN += total
-        else:
-            table.add_row(["TOTAL", f"{TOTAL_BUY:,}", "-", "-", f"{TOTAL_EARN:,}"])
-            messages.append("```\n" + str(table) + "\n```")
-        send_discord_messages(WEBHOOK, messages)
+        main(Discord, browser, target)
 
     except Exception as e:
-        send_discord_message(
-            WEBHOOK,
-            ":warning:" * 10
-            + "ERROR!!!"
-            + ":warning:" * 10
-            + "\n"
-            + "```\n"
-            + str(e)
-            + "\n```",
-        )
+        Discord.message(":warning:" * 10 + "ERROR!!!" + ":warning:" * 10)
+        Discord.message(str(e), codeblock=True)
