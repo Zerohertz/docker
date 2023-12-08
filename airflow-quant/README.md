76e8d9b87dc3406309b381065ef393bc6640f6ff
Zerohertz <ohg3417@gmail.com>
2023-12-08 21:58:24 +0900

    :fire: Add: Airflow Quant

---
+++ airflow-quant/README.md
@@ -0,0 +1,5 @@
+<div align = "center">
+    <a href = "https://hub.docker.com/repository/docker/zerohertzkr/airflow-quant/general">
+        <img src="https://img.shields.io/docker/v/zerohertzkr/airflow-quant?style=for-the-badge&logo=Docker&label=zerohertzkr/airflow-quant&labelColor=800a0a"/>
+    </a>
+</div>
---
+++ airflow-quant/main.py
@@ -0,0 +1,34 @@
+import os
+
+import zerohertzLib as zz
+
+SYMBOLS = int(os.environ.get("SYMBOLS"))
+SLACK = os.environ.get("SLACK")
+START_DAY = os.environ.get("START_DAY")
+TOP = int(os.environ.get("TOP"))
+KOR = bool(int(os.environ.get("KOR")))
+
+if __name__ == "__main__":
+    try:
+        qsb = zz.quant.QuantSlackBotFDR(
+            SYMBOLS,
+            token=SLACK,
+            channel="stock",
+            start_day=START_DAY,
+            top=TOP,
+            name="Stock",
+            icon_emoji="chart_with_upwards_trend",
+            mp_num=6,
+            kor=KOR,
+        )
+        qsb.buy()
+    except Exception as e:
+        qsb.message(
+            ":warning:" * 3
+            + "ERROR!!!"
+            + ":warning:" * 3
+            + "\n"
+            + "```\n"
+            + str(e)
+            + "\n```",
+        )
