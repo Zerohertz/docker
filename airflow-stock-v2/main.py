9ed8c1f958c2d6834bac551932acc9f7e5d4c477
Zerohertz <ohg3417@gmail.com>
2023-12-08 21:12:42 +0900

    :hammer: Update: Parameters

--- airflow-stock-v2/main.py
+++ airflow-stock-v2/main.py
@@ -3,18 +3,31 @@ import os
 import zerohertzLib as zz
 
 SLACK = os.environ.get("SLACK")
+START_DAY = os.environ.get("START_DAY")
+MP_NUM = int(os.environ.get("MP_NUM"))
 KOR = bool(int(os.environ.get("KOR")))
 
 if __name__ == "__main__":
-    qsb = zz.quant.QuantSlackBotKI(
-        [],
-        token=SLACK,
-        channel="zerohertz",
-        start_day="20200101",
-        path="stock",
-        name="Stock",
-        icon_emoji="chart_with_upwards_trend",
-        mp_num=6,
-        kor=KOR,
-    )
-    qsb.sell()
+    try:
+        qsb = zz.quant.QuantSlackBotKI(
+            [],
+            token=SLACK,
+            channel="stock",
+            start_day=START_DAY,
+            path="stock",
+            name="Stock",
+            icon_emoji="chart_with_upwards_trend",
+            mp_num=6,
+            kor=KOR,
+        )
+        qsb.sell()
+    except Exception as e:
+        qsb.message(
+            ":warning:" * 3
+            + "ERROR!!!"
+            + ":warning:" * 3
+            + "\n"
+            + "```\n"
+            + str(e)
+            + "\n```",
+        )
