9e0d6a2d86de67da2f03e68f485a193dc09035fb
Zerohertz <ohg3417@gmail.com>
2023-12-11 09:12:04 +0900

    :sparkles: Add: Airflow Balance

---
+++ airflow-balance/main.py
@@ -0,0 +1,28 @@
+import os
+
+import zerohertzLib as zz
+
+SLACK = os.environ.get("SLACK")
+KOR = bool(int(os.environ.get("KOR")))
+
+if __name__ == "__main__":
+    try:
+        balance = zz.quant.Balance(path="stock", kor=KOR)
+        path = balance.table()
+        slack = zz.api.SlackBot(
+            SLACK,
+            channel="zerohertz",
+            name="Stock",
+            icon_emoji="chart_with_upwards_trend",
+        )
+        slack.file(path)
+    except Exception as e:
+        slack.message(
+            ":warning:" * 3
+            + "ERROR!!!"
+            + ":warning:" * 3
+            + "\n"
+            + "```\n"
+            + str(e)
+            + "\n```",
+        )
