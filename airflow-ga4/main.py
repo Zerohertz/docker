a00cbeabb8b79aee23013ef314d52bd74c54fcce
Zerohertz <ohg3417@gmail.com>
2023-12-11 14:27:07 +0900

    :hammer: Update: GA4

--- airflow-ga4/main.py
+++ airflow-ga4/main.py
@@ -1,15 +1,17 @@
 import json
 import os
 
-import zerohertzLib as zz
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 from matplotlib import pyplot as plt
+
+import zerohertzLib as zz
 
 KEY = json.loads(os.environ.get("KEY"))
 PROPERTY_ID = os.environ.get("PROPERTY_ID")
 SLACK = os.environ.get("SLACK")
 PER = os.environ.get("PER")
+ETC_PAGE = 4
 
 
 def get_ga4_service(KEY):
@@ -35,52 +37,46 @@ def get_data(tar):
     return response
 
 
-def make_barv(title, response, slack):
+def make_barh(title, response, slack):
     tmp_people = {}
     tmp_time = {}
-    for row in response["rows"]:
-        if "zerohertz.github.io" in row["dimensionValues"][0]["value"]:
-            continue
+    for row in response["rows"][::-1]:
         key = (
             row["dimensionValues"][0]["value"]
             .replace(" | Zerohertz", "")
-            .replace("https://", "")[:20]
+            .replace("https://", "")
         )
         tmp_people[key] = int(row["metricValues"][0]["value"])
         tmp_time[key] = round(float(row["metricValues"][1]["value"]))
     peop = {}
     time = {}
-    etc_p = etc_t = 0
-    for key, value in tmp_people.items():
+    max_ylab = 0
+    for i, (key, value) in enumerate(tmp_people.items()):
         if int(PER) > 1:
-            if value >= 5:
+            if value > ETC_PAGE:
                 peop[key] = value
-                time[key] = tmp_time[key]
-            else:
-                etc_p += value
-                etc_t += tmp_time[key]
+                time["\n" * i] = tmp_time[key]
+                max_ylab = max(max_ylab, len(key))
         else:
             peop[key] = value
-            time[key] = tmp_time[key]
-    if int(PER) > 1:
-        peop["Etc"], time["Etc"] = etc_p, etc_t
-    try:
-        zz.plot.figure((30, 10))
-        plt.subplot(1, 2, 1)
-        zz.plot.barv(peop, title, "Number of People", "People", rot=45, save=False)
-        plt.subplot(1, 2, 2)
-        zz.plot.barv(time, title, "Time [sec]", "Time", rot=45, save=False)
-        zz.plot.savefig(title, 100)
-        slack.message(f"> :rocket: {title}")
-        slack.file(f"{title.lower().replace(' ', '_')}.png")
-    except Exception as e:
-        slack.message("Error" + str(e) + f"\npeop: {peop}\ntime: {time}", True)
+            time["\n" * i] = tmp_time[key]
+            max_ylab = max(max_ylab, len(key))
+    zz.plot.figure((max(15, max_ylab // 3), max(10, 3 + int(len(peop) / 2.4))))
+    plt.subplot(1, 2, 1)
+    zz.plot.barh(
+        peop, title, "Number of People", "People", rot=0, per=False, save=False
+    )
+    plt.subplot(1, 2, 2)
+    zz.plot.barh(time, title, "Time [sec]", "Time", rot=0, per=False, save=False)
+    path = zz.plot.savefig(title, 100)
+    slack.message(f"> :rocket: {title}")
+    slack.file(path)
 
 
 def main(tar, slack):
     for t, tit in tar.items():
         response = get_data(t)
-        make_barv(tit, response, slack)
+        make_barh(tit, response, slack)
 
 
 if __name__ == "__main__":
