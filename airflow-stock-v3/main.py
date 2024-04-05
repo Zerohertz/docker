f473b0d99a24297e3a531d9668cb5f18da9b1e3e
Zerohertz <ohg3417@gmail.com>
2024-04-05 21:39:39 +0900

    :fire: Update: Stock Balance v3

--- airflow-stock-v3/main.py
+++ airflow-stock-v3/main.py
@@ -1,39 +1,17 @@
 import os
 import time
 import traceback
-from datetime import datetime, timedelta
+from datetime import datetime
 
-import FinanceDataReader as fdr
 import pytz
 import zerohertzLib as zz
-from matplotlib import pyplot as plt
-from matplotlib import ticker
 
 SLACK = os.environ.get("SLACK")
 NORMAL = os.environ.get("NORMAL")
 ISA = os.environ.get("ISA")
 
 
-def _exchange():
-    now = datetime.now()
-    data = fdr.DataReader("USD/KRW", now - timedelta(days=10))
-    return data.Close[-1]
-
-
-def _merge(balance_1, balance_2, exchange=None):
-    if exchange:
-        for key, value in balance_2.items():
-            balance_2.balance["stock"][key][1] = value[1] * exchange
-            balance_2.balance["stock"][key][2] = value[2] * exchange
-            balance_2.balance["stock"][key][-1] = value[-1] * exchange
-        balance_2.balance["cash"] = balance_2.balance["cash"] * exchange
-    balance_1.balance["stock"] = dict(list(balance_1.items()) + list(balance_2.items()))
-    balance_1.balance["cash"] += balance_2.balance["cash"]
-    return balance_1
-
-
 def _balance():
-    exchange = _exchange()
     try:
         balance = zz.quant.Balance(ISA, path="stock/ISA")
     except KeyError:
@@ -41,17 +19,8 @@ def _balance():
     if balance is None:
         balance = zz.quant.Balance(NORMAL, path="stock/NORMAL")
     else:
-        balance = _merge(balance, zz.quant.Balance(NORMAL, path="stock/NORMAL"))
-    balance = _merge(
-        balance, zz.quant.Balance(NORMAL, path="stock/NORMAL", kor=False), exchange
-    )
-    balance.balance["stock"] = dict(
-        sorted(
-            balance.balance["stock"].items(),
-            key=lambda item: item[1][1] * item[1][3],
-            reverse=True,
-        )
-    )
+        balance.merge(zz.quant.Balance(NORMAL, path="stock/NORMAL"))
+    balance.merge(zz.quant.Balance(NORMAL, path="stock/NORMAL", kor=False))
     return balance
 
 
@@ -66,24 +35,7 @@ def main(slack):
     data.append(balance.balance)
     data[-1]["time"] = str(now)
     zz.util.write_json(data, "stock/balance")
-    data_bar = {}
-    for key, value in data[-1]["stock"].items():
-        data_bar[key] = value[5]
-    zz.plot.figure((20, 10))
-    zz.plot.barv(
-        data_bar,
-        xlab="",
-        ylab="Profit and Loss (P&L) [₩]",
-        title="",
-        rot=45,
-        dim="",
-        dimsize=16,
-        save=False,
-    )
-    plt.gca().yaxis.set_major_formatter(
-        ticker.FuncFormatter(lambda x, p: format(int(x), ","))
-    )
-    path_bar = zz.plot.savefig("bar", 100)
+    path_bar = balance.barv()
     path_balance, path_portfolio = balance.table(), balance.pie()
     if path_balance is None:
         slack.message("Balance: NULL", True)
